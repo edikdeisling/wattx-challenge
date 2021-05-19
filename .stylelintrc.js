d711f471c0240176be4a9a705a48f9e88f9a3f14
Deisling Eduard <eduard.deisling@insolar.io>
2021-05-19 20:24:27 +0300

    add editorconfig, eslint, prettier, stylelint,

---
+++ .stylelintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+	extends: ['stylelint-prettier/recommended', 'stylelint-config-rational-order'],
+	rules: {
+		'comment-empty-line-before': null, // allow not empty line before comment
+		'rule-empty-line-before': "always", // empty lines before css rules
+		'max-nesting-depth': [
+			3, // max css nesting depth
+			{
+				ignore: ['pseudo-classes'],
+				ignoreAtRules: ['mixin', 'keyframes'],
+			},
+		],
+		'length-zero-no-unit': true, // prefer 0. Not 0px
+		'declaration-block-no-duplicate-properties': true, // no css rule duplicates
+		'declaration-empty-line-before': [
+			'always', // add empty line before css classes
+			{
+				ignore: ['after-comment', 'after-declaration', 'first-nested'],
+			},
+		],
+		'order/order': ['custom-properties', 'declarations'], // order. custom-props first
+	},
+};
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,7 +16,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'wattx-challenge'`, () => {
+  it("should have as title 'wattx-challenge'", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('wattx-challenge');
@@ -30,6 +26,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('wattx-challenge app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'wattx-challenge app is running!',
+    );
   });
 });
